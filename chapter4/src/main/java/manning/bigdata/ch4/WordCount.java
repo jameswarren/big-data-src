8e4bf29aa21e022486649b7051433471d2646083
James Warren <james.warren@binatechnologies.com>
2013-11-23 00:23:29 -0800

    use sequence files, explicit key/value class declarations

--- chapter4/src/main/java/manning/bigdata/ch4/WordCount.java
+++ chapter4/src/main/java/manning/bigdata/ch4/WordCount.java
@@ -21,50 +21,50 @@ import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 
 public class WordCount {
-	
-	public static class WordCountMapper extends MapReduceBase implements
-			Mapper<LongWritable, Text, Text, IntWritable> {
-		private static final IntWritable one = new IntWritable(1);
-		private Text word = new Text();
 
-		public void map(LongWritable key, Text value,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			String line = value.toString();
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			while (tokenizer.hasMoreTokens()) {
-				word.set(tokenizer.nextToken());
-				output.collect(word, one);
-			}
-		}
-	}
+  public static class WordCountMapper extends MapReduceBase implements
+  Mapper<LongWritable, Text, Text, IntWritable> {
+    private static final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 
-	public static class WordCountReducer extends MapReduceBase implements
-			Reducer<Text, IntWritable, Text, IntWritable> {
-		public void reduce(Text key, Iterator<IntWritable> values,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			int sum = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
-			}
-			output.collect(key, new IntWritable(sum));
-		}
-	}
+    public void map(LongWritable key, Text value,
+        OutputCollector<Text, IntWritable> output, Reporter reporter)
+            throws IOException {
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      while (tokenizer.hasMoreTokens()) {
+        word.set(tokenizer.nextToken());
+        output.collect(word, one);
+      }
+    }
+  }
 
-	public static void main(String[] args) throws Exception {
-		JobConf conf = new JobConf(WordCount.class);
-		conf.setJobName("wordcount");
-		conf.setMapperClass(WordCountMapper.class);
-		conf.setReducerClass(WordCountReducer.class);
-		conf.setNumReduceTasks(3);
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
-		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-		JobClient.runJob(conf);
-	}
+  public static class WordCountReducer extends MapReduceBase implements
+  Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterator<IntWritable> values,
+        OutputCollector<Text, IntWritable> output, Reporter reporter)
+            throws IOException {
+      int sum = 0;
+      while (values.hasNext()) {
+        sum += values.next().get();
+      }
+      output.collect(key, new IntWritable(sum));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    JobConf conf = new JobConf(WordCount.class);
+    conf.setJobName("wordcount");
+    conf.setMapperClass(WordCountMapper.class);
+    conf.setReducerClass(WordCountReducer.class);
+    conf.setNumReduceTasks(3);
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    JobClient.runJob(conf);
+  }
 
 }
--- chapter4/src/main/java/manning/bigdata/ch4/WordFrequencyVsLength.java
+++ chapter4/src/main/java/manning/bigdata/ch4/WordFrequencyVsLength.java
@@ -23,113 +23,128 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.SequenceFileInputFormat;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 
+@SuppressWarnings("serial")
 public class WordFrequencyVsLength {
 
-	public static class SplitAndFilterMapper extends MapReduceBase
-	 implements Mapper<LongWritable, Text, Text, IntWritable> {
+  public static class SplitAndFilterMapper extends MapReduceBase
+  implements Mapper<LongWritable, Text, Text, IntWritable> {
 
-		public static final Set<String> STOP_WORDS = new HashSet<String>() {{
-			add("the");
-			add("a");
-		}};
-			
-		private static final IntWritable one = new IntWritable(1);
-		private Text word = new Text();
-	 
-		public void map(LongWritable key, Text value,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			String line = value.toString();
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			while (tokenizer.hasMoreTokens()) {
-				String token = tokenizer.nextToken();
-				if (!STOP_WORDS.contains(token)) {
-					word.set(token);
-					output.collect(word, one);
-				}
-			}
-		}
-	}
-	
-	public static class LengthToCountReducer extends MapReduceBase implements
-			Reducer<Text, IntWritable, IntWritable, IntWritable> {
-		
-		public void reduce(Text key, Iterator<IntWritable> values,
-				OutputCollector<IntWritable, IntWritable> output, Reporter reporter)
-				throws IOException {
-			int sum = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
-			}
-			output.collect(new IntWritable(key.toString().length()), new IntWritable(
-					sum));
-		}
-	}
-	
-	public static class PassThroughMapper extends MapReduceBase implements
-			Mapper<IntWritable, IntWritable, IntWritable, IntWritable> {
-		
-		public void map(IntWritable key, IntWritable count,
-				OutputCollector<IntWritable, IntWritable> output, Reporter reporter)
-				throws IOException {
-			output.collect(key, count);
-		}
-	}
-	
-	public static class AverageReducer extends MapReduceBase implements
-			Reducer<IntWritable, IntWritable, IntWritable, DoubleWritable> {
-		public void reduce(IntWritable key, Iterator<IntWritable> values,
-				OutputCollector<IntWritable, DoubleWritable> output, Reporter reporter)
-				throws IOException {
-			int sum = 0;
-			int count = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
-				count += 1;
-			}
-			double avg = 1.0 * sum / count;
-			output.collect(key, new DoubleWritable(avg));
-		}
-	}
-	
-	private static void runWordBucketer( // first job
-			String input, String output) throws Exception {
-		JobConf conf = new JobConf(WordFrequencyVsLength.class);
-		conf.setJobName("freqVsAvg1");
-		conf.setMapperClass(SplitAndFilterMapper.class);
-		conf.setReducerClass(LengthToCountReducer.class);
-		FileInputFormat.setInputPaths(conf, new Path(input));
-		FileOutputFormat.setOutputPath(conf, new Path(output));
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
-		JobClient.runJob(conf);
-	}
+    public static final Set<String> STOP_WORDS = new HashSet<String>() {{
+      add("the");
+      add("a");
+    }};
 
-	private static void runBucketAverager(String input, String output)
-			throws Exception { // second job
-		JobConf conf = new JobConf(WordFrequencyVsLength.class);
-		conf.setJobName("freqVsAvg2");
-		conf.setMapperClass(PassThroughMapper.class);
-		conf.setReducerClass(AverageReducer.class);
-		FileInputFormat.setInputPaths(conf, new Path(input));
-		FileOutputFormat.setOutputPath(conf, new Path(output));
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-		conf.setOutputKeyClass(IntWritable.class);
-		conf.setOutputValueClass(DoubleWritable.class);
-		JobClient.runJob(conf);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		String tmpPath = "/tmp/" + UUID.randomUUID().toString();
-		runWordBucketer(args[0], tmpPath);
-		runBucketAverager(tmpPath, args[1]);
-		FileSystem.get(new Configuration()).delete(new Path(tmpPath), true);
-	}
-	
+    private static final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value,
+        OutputCollector<Text, IntWritable> output, Reporter reporter)
+            throws IOException {
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      while (tokenizer.hasMoreTokens()) {
+        String token = tokenizer.nextToken();
+        if (!STOP_WORDS.contains(token)) {
+          word.set(token);
+          output.collect(word, one);
+        }
+      }
+    }
+  }
+
+  public static class LengthToCountReducer extends MapReduceBase implements
+    Reducer<Text, IntWritable, IntWritable, IntWritable> {
+
+    public void reduce(Text key, Iterator<IntWritable> values,
+        OutputCollector<IntWritable, IntWritable> output, Reporter reporter)
+            throws IOException {
+      int sum = 0;
+      while (values.hasNext()) {
+        sum += values.next().get();
+      }
+      output.collect(new IntWritable(key.toString().length()), new IntWritable(sum));
+    }
+  }
+
+  public static class PassThroughMapper extends MapReduceBase implements
+  Mapper<IntWritable, IntWritable, IntWritable, IntWritable> {
+
+    public void map(IntWritable key, IntWritable count,
+        OutputCollector<IntWritable, IntWritable> output, Reporter reporter)
+            throws IOException {
+      output.collect(key, count);
+    }
+  }
+
+  public static class AverageReducer extends MapReduceBase implements
+  Reducer<IntWritable, IntWritable, IntWritable, DoubleWritable> {
+    public void reduce(IntWritable key, Iterator<IntWritable> values,
+        OutputCollector<IntWritable, DoubleWritable> output, Reporter reporter)
+            throws IOException {
+      int sum = 0;
+      int count = 0;
+      while (values.hasNext()) {
+        sum += values.next().get();
+        count += 1;
+      }
+      double avg = 1.0 * sum / count;
+      output.collect(key, new DoubleWritable(avg));
+    }
+  }
+
+  private static void runWordBucketer( // first job
+      String input, String output) throws Exception {
+    JobConf conf = new JobConf(WordFrequencyVsLength.class);
+    conf.setJobName("freqVsAvg1");
+    
+    conf.setMapOutputKeyClass(Text.class);
+    conf.setMapOutputValueClass(IntWritable.class);
+    conf.setOutputKeyClass(IntWritable.class);
+    conf.setOutputValueClass(IntWritable.class);
+    
+    conf.setMapperClass(SplitAndFilterMapper.class);
+    conf.setReducerClass(LengthToCountReducer.class);
+
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(SequenceFileOutputFormat.class);
+        
+    FileInputFormat.setInputPaths(conf, new Path(input));
+    FileOutputFormat.setOutputPath(conf, new Path(output));
+
+    JobClient.runJob(conf);
+  }
+
+  private static void runBucketAverager(String input, String output)
+      throws Exception { // second job
+    JobConf conf = new JobConf(WordFrequencyVsLength.class);
+    conf.setJobName("freqVsAvg2");
+    
+    conf.setMapOutputKeyClass(IntWritable.class);
+    conf.setMapOutputValueClass(IntWritable.class);
+    conf.setOutputKeyClass(IntWritable.class);
+    conf.setOutputValueClass(DoubleWritable.class);
+    
+    conf.setMapperClass(PassThroughMapper.class);
+    conf.setReducerClass(AverageReducer.class);
+    
+    conf.setInputFormat(SequenceFileInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+
+    FileInputFormat.setInputPaths(conf, new Path(input));
+    FileOutputFormat.setOutputPath(conf, new Path(output));
+    JobClient.runJob(conf);
+  }
+
+  public static void main(String[] args) throws Exception {
+    String tmpPath = "/tmp/" + UUID.randomUUID().toString();
+    runWordBucketer(args[0], tmpPath);
+    runBucketAverager(tmpPath, args[1]);
+    FileSystem.get(new Configuration()).delete(new Path(tmpPath), true);
+  }
+
 }
